74b9f94e76d3f6aa50703be1c52137e2b37a1e71
kaloyan <milchevkaloian@gmail.com>
2024-02-06 22:24:10 +0200

    changed the max len of the image url field

--- backend/backend/cases/mixins.py
+++ backend/backend/cases/mixins.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class SkinCaseMixin(models.Model):
     name = models.CharField(max_length=50)
-    image_url = models.URLField()
+    image_url = models.URLField(max_length=300)
     price = models.DecimalField(decimal_places=2, max_digits=8)
 
     class Meta:
