c25c89beb47113d989e9bcd5a9cd1818bd2493b7
kaloyan <milchevkaloian@gmail.com>
2024-02-09 00:33:18 +0200

    added cases app urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.api.urls')),
-    path('api/test/', include('backend.test_dev.urls'))
+    path('api/test/', include('backend.test_dev.urls')),
+    path('cases/', include('backend.cases.urls'))
 ]
