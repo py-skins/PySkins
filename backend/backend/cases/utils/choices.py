736ce9447381cea3455e0f74848299fb716eebec
Kaloyan~ <milchevkaloian@gmail.com~>
2024-02-09 21:29:45 +0200

    new package utils with new choices file inside

---
+++ backend/backend/cases/utils/choices.py
@@ -0,0 +1,24 @@
+def get_skin_quality_choices():
+    FACTORY_NEW = "factory new"
+    MINIMAL_WEAR = "minimal wear"
+    FIELD_TESTED = "field-tested"
+    WELL_WORN = "well-worn"
+    BATTLE_SCARRED = "battle-scarred"
+    STATTRAK_FACTORY_NEW = "stattrak factory new"
+    STATTRAK_MINIMAL_WEAR = "stattrak minimal wear"
+    STATTRAK_FIELD_TESTED = "stattrak field-tested"
+    STATTRAK_WELL_WORN = "stattrak well-worn"
+    STATTRAK_BATTLE_SCARRED = "stattrak battle-scarred"
+
+    return (
+        (FACTORY_NEW, FACTORY_NEW),
+        (MINIMAL_WEAR, MINIMAL_WEAR),
+        (FIELD_TESTED, FIELD_TESTED),
+        (WELL_WORN, WELL_WORN),
+        (BATTLE_SCARRED, BATTLE_SCARRED),
+        (STATTRAK_FACTORY_NEW, STATTRAK_FACTORY_NEW),
+        (STATTRAK_MINIMAL_WEAR, STATTRAK_MINIMAL_WEAR),
+        (STATTRAK_FIELD_TESTED, STATTRAK_FIELD_TESTED),
+        (STATTRAK_WELL_WORN, STATTRAK_WELL_WORN),
+        (STATTRAK_BATTLE_SCARRED, STATTRAK_BATTLE_SCARRED),
+    )
