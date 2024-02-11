46e4f6045c512b3cb9f7b9cc2b33603e2acb4c59
Kaloyan~ <milchevkaloian@gmail.com~>
2024-02-11 22:51:09 +0200

    cases and skins serializers

--- backend/backend/cases/serializers.py
+++ backend/backend/cases/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
-from backend.cases.models import Case
+from backend.cases.models import Case, Skin
+
+
+class SkinSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skin
+        fields = '__all__'
 
 
 class CaseSerializer(serializers.ModelSerializer):
