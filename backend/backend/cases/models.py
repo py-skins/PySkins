0e966d139577c23788d531ce9e9f3af5a821239f
Kaloyan~ <milchevkaloian@gmail.com~>
2024-02-11 22:50:51 +0200

    moved choices in another file

--- backend/backend/cases/models.py
+++ backend/backend/cases/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-from backend.cases.mixins import SkinCaseMixin
+from backend.cases.utils.choices import get_skin_quality_choices
+from backend.cases.utils.mixins import SkinCaseMixin
 
 
 class Case(SkinCaseMixin):
@@ -8,29 +9,7 @@ class Case(SkinCaseMixin):
 
 
 class Skin(SkinCaseMixin):
-    FACTORY_NEW = "factory new"
-    MINIMAL_WEAR = "minimal wear"
-    FIELD_TESTED = "field-tested"
-    WELL_WORN = "well-worn"
-    BATTLE_SCARRED = "battle-scarred"
-    STATTRAK_FACTORY_NEW = "stattrak factory new"
-    STATTRAK_MINIMAL_WEAR = "stattrak minimal wear"
-    STATTRAK_FIELD_TESTED = "stattrak field-tested"
-    STATTRAK_WELL_WORN = "stattrak well-worn"
-    STATTRAK_BATTLE_SCARRED = "stattrak battle-scarred"
-
-    SKIN_QUALITY_CHOICES = (
-        (FACTORY_NEW, FACTORY_NEW),
-        (MINIMAL_WEAR, MINIMAL_WEAR),
-        (FIELD_TESTED, FIELD_TESTED),
-        (WELL_WORN, WELL_WORN),
-        (BATTLE_SCARRED, BATTLE_SCARRED),
-        (STATTRAK_FACTORY_NEW, STATTRAK_FACTORY_NEW),
-        (STATTRAK_MINIMAL_WEAR, STATTRAK_MINIMAL_WEAR),
-        (STATTRAK_FIELD_TESTED, STATTRAK_FIELD_TESTED),
-        (STATTRAK_WELL_WORN, STATTRAK_WELL_WORN),
-        (STATTRAK_BATTLE_SCARRED, STATTRAK_BATTLE_SCARRED),
-    )
+    SKIN_QUALITY_CHOICES = get_skin_quality_choices()
 
     quality = models.CharField(max_length=30, choices=SKIN_QUALITY_CHOICES)
     rarity_color = models.CharField(max_length=10)
