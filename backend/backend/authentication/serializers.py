4e905f36de81e6edfc396342150921a928d9f223
kaloyan <milchevkaloian@gmail.com>
2024-03-10 11:03:15 +0200

    changed the authentication to jwt

--- backend/backend/authentication/serializers.py
+++ backend/backend/authentication/serializers.py
@@ -1,24 +1,51 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 UserModel = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=UserModel.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = UserModel
-        fields = ('id', 'email', 'password')
+        fields = ('email', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match"}
+            )
+        return attrs
 
     def create(self, validated_data):
-        user = UserModel.objects.create_user(
+        user = UserModel.objects.create(
             email=validated_data['email'],
-            password=validated_data['password']
         )
+        user.set_password(validated_data['password'])
+        user.save()
         return user
 
 
-class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
+
+
+class AuthUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = '__all__'
--- backend/backend/authentication/urls.py
+++ backend/backend/authentication/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from backend.authentication import views
 
 urlpatterns = (
-    path('register/', views.RegisterAPIView.as_view()),
-    path('login/', views.LoginAPIView.as_view()),
-    path('logout/', views.LogoutAPIView.as_view()),
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterAPIView.as_view(), name='register'),
+    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
+    path('profile/', views.get_profile, name='profile'),
 )
