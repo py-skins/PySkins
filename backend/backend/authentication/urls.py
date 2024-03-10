bf1e9091edcda80dc575761d4bfa19b12eed083d
kaloyan <milchevkaloian@gmail.com>
2024-03-10 11:11:10 +0200

    removed profile and logout urls

--- backend/backend/authentication/urls.py
+++ backend/backend/authentication/urls.py
@@ -7,6 +7,4 @@ urlpatterns = (
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterAPIView.as_view(), name='register'),
-    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
-    path('profile/', views.get_profile, name='profile'),
 )
