d2495c8951e4fb265aa2b892a04cf7409cd53bc6
Kaloyan~ <milchevkaloian@gmail.com~>
2024-04-05 22:57:33 +0300

    removed skin models

--- backend/backend/cases/models.py
+++ backend/backend/cases/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.text import slugify
 
 from backend.accounts.models import Account
-from backend.cases.utils.choices import get_skin_quality_chance_choices
 from backend.cases.utils.mixins import SkinCaseBaseMixin
 
 
@@ -13,18 +12,3 @@ class Case(SkinCaseBaseMixin):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
-
-
-class SkinBase(SkinCaseBaseMixin):
-    rarity_color = models.CharField(max_length=10)
-    weapon_type = models.CharField(max_length=20)
-    preview_image_url = models.URLField()
-    case_container = models.ForeignKey(Case, related_name='base_skins', on_delete=models.DO_NOTHING)
-
-
-class Skin(models.Model):
-    SKIN_QUALITY_CHOICES = get_skin_quality_chance_choices()
-    quality = models.CharField(max_length=30, choices=SKIN_QUALITY_CHOICES)
-    price = models.DecimalField(decimal_places=2, max_digits=8, default=0)
-    owner = models.ForeignKey(Account, on_delete=models.CASCADE)
-    base_skin = models.ForeignKey(SkinBase, on_delete=models.CASCADE)
