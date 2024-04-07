a0cdff8163f85b5319ada77fd0e3188daf4fefa3
Kaloyan~ <milchevkaloian@gmail.com~>
2024-04-07 17:55:59 +0300

    added choices for drop skin by color rarity

--- backend/backend/skins/utils/choices.py
+++ backend/backend/skins/utils/choices.py
@@ -1,14 +1,14 @@
 def get_skin_quality_chance_choices():
-    FACTORY_NEW = "factory new"
-    MINIMAL_WEAR = "minimal wear"
-    FIELD_TESTED = "field-tested"
-    WELL_WORN = "well-worn"
-    BATTLE_SCARRED = "battle-scarred"
-    STATTRAK_FACTORY_NEW = "stattrak factory new"
-    STATTRAK_MINIMAL_WEAR = "stattrak minimal wear"
-    STATTRAK_FIELD_TESTED = "stattrak field-tested"
-    STATTRAK_WELL_WORN = "stattrak well-worn"
-    STATTRAK_BATTLE_SCARRED = "stattrak battle-scarred"
+    FACTORY_NEW = 'factory new'
+    MINIMAL_WEAR = 'minimal wear'
+    FIELD_TESTED = 'field-tested'
+    WELL_WORN = 'well-worn'
+    BATTLE_SCARRED = 'battle-scarred'
+    STATTRAK_FACTORY_NEW = 'stattrak factory new'
+    STATTRAK_MINIMAL_WEAR = 'stattrak minimal wear'
+    STATTRAK_FIELD_TESTED = 'stattrak field-tested'
+    STATTRAK_WELL_WORN = 'stattrak well-worn'
+    STATTRAK_BATTLE_SCARRED = 'stattrak battle-scarred'
 
     return (
         (FACTORY_NEW, 5),
@@ -21,4 +21,18 @@ def get_skin_quality_chance_choices():
         (STATTRAK_FIELD_TESTED, 6),
         (STATTRAK_WELL_WORN, 7),
         (STATTRAK_BATTLE_SCARRED, 11),
+    )
+
+
+def get_skin_chance_choices():
+    LIGHT_BLUE = '#4B69FF'
+    BLUE = '#8847FF'
+    PURPLE = '#D32EE6'
+    RED = '#EB4B4B'
+
+    return (
+        (LIGHT_BLUE, 30),
+        (BLUE, 30),
+        (PURPLE, 30),
+        (RED, 5),
     )
