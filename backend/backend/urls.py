8c2787be6e0052345f952698b803ce22f080edc8
Kaloyan~ <milchevkaloian@gmail.com~>
2024-04-23 00:14:46 +0300

    added skins to urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-
-router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('backend.authentication.urls')),
     path('accounts/', include('backend.accounts.urls')),
     path('cases/', include('backend.cases.urls')),
+    path('skins/', include('backend.skins.urls')),
 ]
